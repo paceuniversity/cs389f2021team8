6fc708b744696fb26cdb816ef3f60b700b4e3aa7
Benjamin Fein <benaaronfein@gmail.com>
2021-11-01 20:37:06 -0400

    Add login system

---
+++ MyTeacher/app/src/main/java/com/pace/myteacher/Home.java
@@ -0,0 +1,33 @@
+package com.pace.myteacher;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
+
+import java.util.Arrays;
+import java.util.List;
+public class Home extends AppCompatActivity  {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        TextView welcomeStr = (TextView) findViewById(R.id.welcomeString);
+        welcomeStr.setText("Welcome, " + user.getEmail());
+    }
+}
