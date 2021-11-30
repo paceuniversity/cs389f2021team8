9f35bf3b6b4adeca5c42eb8ab74272692461b695
Benjamin Fein <benaaronfein@gmail.com>
2021-11-30 09:08:50 -0500

    Add real time syncing

---
+++ MyTeacher/app/src/androidTest/java/com/pace/myteacher/TeacherViewTest.java
@@ -0,0 +1,7 @@
+package com.pace.myteacher;
+
+import junit.framework.TestCase;
+
+public class TeacherViewTest extends TestCase {
+
+}
--- MyTeacher/app/src/main/java/com/pace/myteacher/TeacherView.java
+++ MyTeacher/app/src/main/java/com/pace/myteacher/TeacherView.java
@@ -2,11 +2,13 @@ package com.pace.myteacher;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.AsyncTaskLoader;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import android.util.Log;
@@ -37,13 +39,18 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.TimeZone;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class TeacherView extends AppCompatActivity  {
+public class TeacherView extends AppCompatActivity {
     private static final String TAG = "TeacherView";
     String avgRating = "";
     private RecyclerView reviewsRecyclerView;
     private ReviewItemAdapter adapter;
     private List<ReviewItem> reviewItemList = new ArrayList<>();
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -61,60 +68,6 @@ public class TeacherView extends AppCompatActivity  {
         reviewsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
 //        TableLayout tl = (TableLayout) findViewById(R.id.teacherTable);
-
-        Log.d(TAG,"Teacher Review Loaded");
-
-        db.collection("reviews")
-                .whereEqualTo("teacherId", getIntent().getStringExtra("teacherUserID"))
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            Log.d(TAG,"Teacher On Success");
-                            double total = 0.00;
-                            NumberFormat formatter = new DecimalFormat("##.###");
-                            Calendar date = Calendar.getInstance();
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-
-                                if (document.toObject(Reviews.class).getPublishTime().compareTo(new Timestamp(date.getTime())) <= 0) {
-                                    try {
-                                        total += Double.parseDouble(document.toObject(Reviews.class).getRating());
-                                    } catch (Exception e) {
-                                        total += 0;
-                                    }
-                                    //create an object of ReviewItem with reviewer,rating and review.
-                                    ReviewItem reviewItem = new ReviewItem(document.toObject(Reviews.class).getReviewer(),
-                                            document.toObject(Reviews.class).getRating(),document.toObject(Reviews.class).getReview());
-                                    Log.d(TAG,"***************  Reviewer : "+reviewItem.getReviewer()+" Rating : "+reviewItem.getRating()+" Review : "+reviewItem.getReview()+"  ****************");
-
-                                    //add the review to the array list
-                                    reviewItemList.add(reviewItem);
-
-                                }
-                                total = total / task.getResult().size();
-                                avgRating = "Rating : "+ formatter.format(total);
-                                TextView ratingLabel = (TextView) findViewById(R.id.welcomeString);
-                                ratingLabel.setText(avgRating);
-                            }
-
-                            runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-
-                                    adapter = new ReviewItemAdapter(TeacherView.this, reviewItemList);
-                                    reviewsRecyclerView.setAdapter(adapter);
-                                }
-                            });
-
-                        } else {
-                            System.out.println("Error getting documents: " + task.getException());
-                            Log.d(TAG,"Error getting documents: " + task.getException());
-                        }
-
-
-                    }
-                });
         Intent intentNewReview = new Intent(TeacherView.this, NewReview.class);
         Bundle b = new Bundle();
 
@@ -122,7 +75,67 @@ public class TeacherView extends AppCompatActivity  {
         b.putString("teacherEmail", getIntent().getStringExtra("teacherEmail"));
 
         intentNewReview.putExtras(b);
+        Timer myTimer = new Timer ();
+        TimerTask myTask = new TimerTask() {
+            @Override
+            public void run () {
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
 
+                db.collection("reviews")
+                        .whereEqualTo("teacherId", getIntent().getStringExtra("teacherUserID"))
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    Log.d(TAG, "Teacher On Success");
+                                    double total = 0.00;
+                                    NumberFormat formatter = new DecimalFormat("##.###");
+                                    Calendar date = Calendar.getInstance();
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+
+                                        if (document.toObject(Reviews.class).getPublishTime().compareTo(new Timestamp(date.getTime())) <= 0) {
+                                            try {
+                                                total += Double.parseDouble(document.toObject(Reviews.class).getRating());
+                                            } catch (Exception e) {
+                                                total += 0;
+                                            }
+                                            //create an object of ReviewItem with reviewer,rating and review.
+                                            ReviewItem reviewItem = new ReviewItem(document.toObject(Reviews.class).getReviewer(),
+                                                    document.toObject(Reviews.class).getRating(), document.toObject(Reviews.class).getReview());
+                                            Log.d(TAG, "***************  Reviewer : " + reviewItem.getReviewer() + " Rating : " + reviewItem.getRating() + " Review : " + reviewItem.getReview() + "  ****************");
+
+                                            //add the review to the array list
+                                            reviewItemList.add(reviewItem);
+
+                                        }
+                                        total = total / task.getResult().size();
+                                        avgRating = "Rating : " + formatter.format(total);
+                                        TextView ratingLabel = (TextView) findViewById(R.id.welcomeString);
+                                        ratingLabel.setText(avgRating);
+                                    }
+
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+
+                                            adapter = new ReviewItemAdapter(TeacherView.this, reviewItemList);
+                                            reviewsRecyclerView.setAdapter(adapter);
+                                        }
+                                    });
+
+                                } else {
+                                    System.out.println("Error getting documents: " + task.getException());
+                                    Log.d(TAG, "Error getting documents: " + task.getException());
+                                }
+
+
+                            }
+                        });
+            }
+        };
+
+        myTimer.scheduleAtFixedRate(myTask , 0l, 5 * (60*1000));
         FloatingActionButton newBtn = (FloatingActionButton) findViewById(R.id.newReviewBtn);
         newBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -140,10 +153,12 @@ public class TeacherView extends AppCompatActivity  {
             @Override
             public void onClick(View v) {
 
-                    startActivity(intent);
+                startActivity(intent);
 
             }
         });
+        Log.d(TAG,"Teacher Review Loaded");
+
 
     }
 }
